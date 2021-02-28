64eb1810816a88366a3b5382d86dc435b7df639d
Miguel Rico <miguel35697@hotmail.com>
2021-02-27 20:28:16 -0500

    First commit

---
+++ src/GUI/Frame.java
@@ -0,0 +1,34 @@
+package GUI;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+
+    private final int ancho = 600;
+    private final int alto = 600;
+
+    public Frame() {
+        initTemplate();
+    }
+
+    public void initTemplate() {
+        setLayout(null);
+        setTitle("Parchis");
+        setSize(new Dimension(ancho, alto));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        initComponents();
+        initListeners();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public void initComponents() {
+
+    }
+
+    public void initListeners() {
+
+    }
+
+}
---
+++ src/Main/main.java
@@ -0,0 +1,11 @@
+package Main;
+
+import GUI.Frame;
+
+public class main {
+
+    public static void main(String[] args) {
+        Frame newFrame = new Frame();
+    }
+
+}
