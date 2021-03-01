b00298bb2a126a5152b492524b9416ac601ee6c7
Sergio Paez <48888488+spaezsuarez@users.noreply.github.com>
2021-02-28 19:11:47 -0500

    Create MovimientoStrategy.java

---
+++ src/Interfaces/MovimientoStrategy.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+import java.util.ArrayList;
+import Logic.Ficha;
+
+public interface MovimientoStrategy {
+	
+	public void moverFicha(ArrayList<Ficha> fichas,int valor);
+
+}
